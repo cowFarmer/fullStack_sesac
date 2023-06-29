851ea1baae8602d17fa8f70d4e2e79dbfbac0fd5
hou <41228208+cowFarmer@users.noreply.github.com>
2023-06-29 18:06:21 +0900

    [UPDATE] flask, crm project

--- 3.flask/main.py
+++ 3.flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__, static_folder="static")
@@ -11,6 +11,62 @@ def get_user(id):
             if line["Id"] == id:
                 return line
     return None
+
+# csv 파일의 공백 처리
+@app.route('/space')
+def user_space():
+    data = []
+    with open("./csv/data_space.csv", "r") as file:
+        lines = csv.DictReader(file, skipinitialspace=True)
+        for line in lines:
+            # clean_row = {key.strip: value.strip() for key, value in line.items()}
+            # data.append(clean_row)
+            data.append(line)
+            return data
+    return None
+   
+# csv 파일 읽은 그대로 header를 처리하는 방법   
+@app.route('/header')
+def header():
+    data = []
+    with open("./csv/data_space.csv", "r") as file:
+        lines = csv.DictReader(file, skipinitialspace=True)
+        headers = [header.strip() for header in lines.fieldnames]
+        # # 첫 줄을 header로 가져오는 두 번째 방법
+        # headers = next(lines)
+        for line in lines:
+            data.append(line)
+    return render_template('header.html', headers=headers, datas=data)
+
+@app.route('/big_user/')
+def big_user():
+    page = request.args.get('page', default=1, type=int)
+    search_name = request.args.get('name', default="", type=str)
+    data = []
+    per_page = 5    
+    search_name = search_name.strip()    
+                
+    with open("./csv/user.csv", "r") as file:
+        lines = csv.DictReader(file, skipinitialspace=True)
+        headers = next(lines)
+        
+        if search_name != "":
+            for line in lines:
+                if search_name in line['Name']:
+                    data.append(line)
+        else:
+            for line in lines:
+                data.append(line)
+                
+    total_page = (len(data) // per_page) + 1
+    start_index = per_page * (page - 1)
+    end_index = max(5, start_index + per_page)
+    page_data = data[start_index:end_index]
+    current_min_page_num = max(1, page-2)
+    current_max_page_num = min(total_page, page+2)
+    page_list = [_ for _ in range(current_min_page_num, current_max_page_num+1)]
+    
+    return render_template('big_user.html', headers=headers, datas=page_data, total_page=total_page, page=page, page_list=page_list)
 
 @app.route('/')
 def home():
---
+++ 3.flask/test.py
@@ -0,0 +1,5 @@
+name = "홍길동"
+
+for i in name:
+    if i == "홍":
+        print(name)
