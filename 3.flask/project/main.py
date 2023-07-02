4e9219b0403b7ee25dc265f8be918a4a18acd72c
hou <41228208+cowFarmer@users.noreply.github.com>
2023-07-02 15:06:54 +0900

    [FIX] pagination index 버그 수정

--- 3.flask/project/main.py
+++ 3.flask/project/main.py
@@ -65,7 +65,7 @@ def readFile(filename=None, search_info=None, per_page=None, current_page=None):
                     if all(line.get(key) == value for key, value in search_info_filter.items()):
                         data.append(line)
         
-        total_page = math.ceil((len(data) // per_page))
+        total_page = math.ceil((len(data) / per_page))
         data_start_index = per_page * (current_page - 1)
         data_end_index = data_start_index + per_page
         slice_page_data = data[data_start_index:data_end_index]
@@ -156,28 +156,8 @@ def order_item_list():
     with open("./csv/orderitem.csv", "r") as file:
         lines = csv.reader(file)
         return render_template("order_item.html", users_info = lines)
-    
-# # csv 파일의 공백 처리
-# @app.route('/space')
-# def user_space():
-#     data = []
-#     with open("./csv/data_space.csv", "r") as file:
-#         lines = csv.DictReader(file, skipinitialspace=True)
-#         for line in lines:
-#             data.append(line)
-#             return data
-#     return None
-   
-# # csv 파일 읽은 그대로 header를 처리하는 방법   
-# @app.route('/header')
-# def header():
-#     data = []
-#     with open("./csv/data_space.csv", "r") as file:
-#         lines = csv.DictReader(file, skipinitialspace=True)
-#         headers = next(lines)
-#         for line in lines:
-#             data.append(line)
-#     return render_template('header.html', headers=headers, datas=data)    
+
     
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
+    # app.run(debug=True, port=8080, host='0.0.0.0')
