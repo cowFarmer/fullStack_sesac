d12c7ed0fc30a890a0d66757dda42638d703009a
hou <41228208+cowFarmer@users.noreply.github.com>
2023-07-03 09:41:50 +0900

    [refactory] 리펙토링, 수업용 파일 옮기기

--- 3.flask/project/main.py
+++ 3.flask/project/main.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
 import csv
-import math
-from functions.page.page_function import pageList
 from functions.dict_controller import ReadCsvDict
+from functions.user.user_function import filter_user_file
 
 app = Flask(__name__, static_folder="static")
 
@@ -12,62 +11,6 @@ store_csv_file = "./csv/store.csv"
 item_csv_file = "./csv/item.csv"
 order_csv_file = "./csv/order.csv"
 order_item_csv_file = "./csv/orderitem.csv"
-
-
-
-def readFile(filename=None, search_info=None, per_page=100, current_page=None):
-    data = []
-    search_info_filter = {}
-    headers, lines = ReadCsvDict.file_name(filename=filename)
-        
-    # search_info filtering
-    if search_info != None:
-        for key, value in search_info.items():
-            if value != "":
-                search_info_filter[key] = value
-        if search_info["age_group"] == -10:
-            search_info_filter.pop("age_group")
-    
-    if len(search_info_filter) == 0 or search_info == None:
-        for line in lines:
-            data.append(line)
-    else:
-        # name 먼저 필터링
-        if "name" in search_info_filter.keys():
-            data = []
-            for line in lines:
-                if search_info_filter["name"] in line["name"]:
-                    data.append(line)
-            search_info_filter.pop("name")
-            lines = data
-        
-        # group_age 필터링
-        if "age_group" in search_info_filter.keys():
-            data = []
-            age_group_value = search_info_filter.get("age_group")
-            age_filter = {"age_group": [str(_) for _ in range(age_group_value, age_group_value+10)]}
-            
-            for line in lines:
-                if line["age"] in age_filter["age_group"]:
-                    data.append(line)
-            search_info_filter.pop("age_group")
-            lines = data
-        
-        # value가 같은 경우 모두 비교
-        if len(search_info_filter) > 0:
-            data = []
-            for line in lines:
-                if all(line.get(key) == value for key, value in search_info_filter.items()):
-                    data.append(line)
-    
-    total_page = math.ceil((len(data) / per_page))
-    data_start_index = per_page * (current_page - 1)
-    data_end_index = data_start_index + per_page
-    slice_page_data = data[data_start_index:data_end_index]
-    page_list = pageList(total_page, current_page)
-        
-    return headers, total_page, slice_page_data, page_list
-        
 
 @app.route('/')
 def home():
@@ -82,7 +25,6 @@ def user():
     
     # GET
     current_page = request.args.get("page", default=1, type=int)
-    # 검색 공백 제거 .strip() 추가
     search_name = request.args.get("name", default="", type=str).strip()
     search_gender = request.args.get("gender", default="", type=str)
     search_age_group = request.args.get("age_group", default=-10, type=int)
@@ -92,7 +34,7 @@ def user():
     search_info["gender"] = search_gender
     search_info["age_group"] = search_age_group
 
-    headers, total_page, slice_page_data, page_list = readFile(filename=user_csv_file, search_info=search_info, per_page=per_page, current_page=current_page)
+    headers, total_page, slice_page_data, page_list = filter_user_file(filename=user_csv_file, search_info=search_info, per_page=per_page, current_page=current_page)
     return render_template('main.html', headers=headers, datas=slice_page_data, total_page=total_page, page_list=page_list, 
                            page=current_page, search_name=search_name, search_gender=search_gender, search_age_group=search_age_group)
 
