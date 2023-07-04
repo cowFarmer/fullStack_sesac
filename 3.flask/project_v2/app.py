a83793bee5246f4af947799b19eb97c51ca80da4
hou <41228208+cowFarmer@users.noreply.github.com>
2023-07-04 21:12:05 +0900

    [BUG FIX] 나이대 range discription 추가, 호버 포인터 제거, 검색창 한글

--- 3.flask/project_v2/app.py
+++ 3.flask/project_v2/app.py
@@ -76,7 +76,6 @@ def user_info(id):
 def store_list():
     headers, lines = ReadCsvDict.file_name(filename=store_csv_file)
     datas = CheckData().check(lines)
-    print("hello")
     return render_template("store_list.html", headers=headers, datas=datas)
 
 @app.route('/store/<id>')
@@ -87,12 +86,20 @@ def store_info(id):
     ordered_data = []
     order_headers, order_lines = ReadCsvDict.file_name(filename=order_csv_file)
     ordered_data = CheckData().check_same_feature(datas, "id", order_lines, "storeid")
-
-    for i in headers:
-        if i == "id":
-            print("id")
-    return render_template("store_order_info.html", headers=headers, datas=datas, 
-                           order_headers=order_headers, ordered_data=ordered_data)
+    
+    # 오더 유저 아이디와 오더 아이템 비교
+    _, order_item_lines = ReadCsvDict.file_name(filename=order_item_csv_file)
+    ordered_item = CheckData().check_same_feature(ordered_data, "orderid", order_item_lines, "orderid")
+    
+    # 오더 아이템과 아이템 비교
+    _, item_lines = ReadCsvDict.file_name(filename=item_csv_file)
+    items = CheckData().check_same_feature(ordered_item, "itemid", item_lines, "id")
+    
+    total_price = CheckData().total_price(items)
+    
+    return render_template("store_info.html", headers=headers, datas=datas, 
+                           order_headers=order_headers, ordered_data=ordered_data,
+                           total_price=total_price)
         
 @app.route('/item')
 def item_list():
