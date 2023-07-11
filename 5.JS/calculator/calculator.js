81c7b07b8eca108debb6c2f7669ce0770aa9f292
hou <41228208+cowFarmer@users.noreply.github.com>
2023-07-11 19:35:50 +0900

    [U] 계산기 마무리

--- 5.JS/calculator/calculator.js
+++ 5.JS/calculator/calculator.js
@@ -1,32 +1,45 @@
 const input_field = document.getElementById("input_field");
 const input_buttons = document.querySelectorAll("button");
 
+const valid_input = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "+", "-", "/", "=", "Backspace"];
+const enter = ["Enter", "="];
+const cancle = ["Escape", "C"];
+
 function cal_result(data) {
+    /*
+    계산
+    */
     return eval(data);
 }
 
 input_field.addEventListener("keydown", function(e) {
-    const valid_input = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "+", "-", "/", "=", "Backspace"];
-    const enter = ["Enter"];
-    const cancle = ["Escape", "C"];
+    /*
+    input_field keydown event handler
+    */
     const keyPressed = e.key;
 
-    if (enter.includes(keyPressed)) {
+    if (valid_input.includes(keyPressed)) {
+        // pass
+    } else if (enter.includes(keyPressed)) {
         input_field.value = cal_result(input_field.value);
-    }
-
-    if (cancle.includes(keyPressed)) {
+    } else if (cancle.includes(keyPressed)) {
         input_field.value = ""
-    }
-
-    // 입력 금지
-    if (!valid_input.includes(keyPressed) && !enter.includes(keyPressed)) {
+    } else {
         e.preventDefault();
     }
 });
 
 input_buttons.forEach(function(button) {
+    /*
+    button click event handler
+    */
     button.addEventListener("click", function (e) {
-        input_field.value += button.textContent;
+        if (enter.includes(button.textContent)) {
+            input_field.value = cal_result(input_field.value);
+        } else if (cancle.includes(button.textContent)) {
+            input_field.value = "";
+        } else {
+            input_field.value += button.textContent;
+        } 
     });
 });
