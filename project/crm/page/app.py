cec9f3f5829f20dcfd963762e3ee203543184980
hou <41228208+cowFarmer@users.noreply.github.com>
2023-07-13 17:33:18 +0900

    [U] page navbar 적용

--- project/crm/page/app.py
+++ project/crm/page/app.py
@@ -1,27 +1,31 @@
-from flask import Flask, render_template, request, Blueprint
-
-from utils.static_url import user_csv_file, order_csv_file, order_item_csv_file, item_csv_file, store_csv_file
-from utils.controller import ReadCsvDict, CheckData
-
-from routes.user.views import user_bp
-from routes.store.views import store_bp
-from routes.item.views import item_bp
-from routes.order.views import order_bp
-from routes.order_item.views import order_item
+from flask import Flask, render_template
 
 
 app = Flask(__name__, static_folder="static")
 
-app.register_blueprint(user_bp)
-app.register_blueprint(store_bp)
-app.register_blueprint(item_bp)
-app.register_blueprint(order_bp)
-app.register_blueprint(order_item)
-
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route("/user")
+def user():
+    return render_template("user.html")
+
+@app.route("/store")
+def store():
+    return render_template("store.html")
+
+@app.route("/item")
+def item():
+    return render_template("item.html")
+
+@app.route("/order")
+def order():
+    return render_template("order.html")
+
+@app.route("/order_item")
+def order_item():
+    return render_template("order_item.html")
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8888)
-    # app.run(debug=True, port=8888, host='0.0.0.0')
+    app.run(debug=True, port=8080)
--- project/crm/page/utils/page/page_function.py
+++
@@ -1,9 +0,0 @@
-# pageList return function
-def pageList(total_page, current_page):
-    if current_page <= 2:
-        page_list = [_ for _ in range(1, min(total_page+1, 6))]
-    elif current_page == total_page or current_page == total_page - 1:
-        page_list = [_ for _ in range(max(1, total_page-4), total_page+1)]
-    else:
-        page_list = [_ for _ in range(current_page-2, current_page+2+1)]
-    return page_list
