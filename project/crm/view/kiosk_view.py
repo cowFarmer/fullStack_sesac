d8d9c8565b2d8fad1bffdeb59e5b2a3f751275cf
hou <41228208+cowFarmer@users.noreply.github.com>
2023-07-21 09:53:33 +0900

    kiosk 1차 TODO 완료

--- project/crm/view/kiosk_view.py
+++ project/crm/view/kiosk_view.py
@@ -8,7 +8,7 @@ kiosk_bp = Blueprint('kiosk', __name__)
 
 @kiosk_bp.route("/kiosk")
 def kiosk():
-    # TODO 
+    # TODO 1차
     # 1. store 정보 가져오기
     # 2. store 선택하여 kiosk_store로 이동하기
     query_util = QueryUtil()
@@ -21,7 +21,7 @@ def kiosk():
 
 @kiosk_bp.route("/kiost/store=<id>")
 def kiosk_store(id):
-    # TODO
+    # TODO 2차
     # 3. store에 있는 메뉴 조회할 수 있게 하기
     # 4. 메뉴 +- 버튼 만들기
     # 5. submit 버튼 만들기
