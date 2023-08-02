e24db1955c7fb994f7a5df276c645c1ed243147d
hou <41228208+cowFarmer@users.noreply.github.com>
2023-08-02 14:16:23 +0900

    django todo app 만들기 study

--- 12.django/HelloWorld2/HelloWorld2/urls.py
+++ 12.django/HelloWorld2/HelloWorld2/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('helloapp.urls'))
+    path('', include('helloapp.urls')),
+    path('todo/', include('todo.urls'))
 ]
