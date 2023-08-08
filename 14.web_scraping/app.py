722db977411501dda4045e74a38f0657cbddcee0
hou <41228208+cowFarmer@users.noreply.github.com>
2023-08-08 21:19:14 +0900

    Feat: movie 데이터 조회, 스크랩핑 DB 추가 - daily DB table 추가 - daily DB scraping 추가 - DB 쿼리 조회 후 데이터 전송 추가 - daily DB url 접속 시 조회

--- 14.web_scraping/app.py
+++ 14.web_scraping/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from database import Database
-from movie import get_movie_data
+from movie import get_movie_ranking_data
 
 
 app = Flask(__name__)
@@ -8,22 +8,92 @@ db = Database()
 
 @app.route('/')
 def index():
-    query = "SELECT * FROM movie_data"
+    query = "SELECT * FROM movie_data;"
     result = db.execute_fetch(query)
-    return render_template('index.html', result=result)
+    datas = []
+    for d in result:
+        d = {
+            'id': d[0], 
+            'name': d[1], 
+            'grade': d[2],
+            'reservation_percente': d[3], 
+            'img_url': d[4], 
+            'short_summary': d[5], 
+            'url': d[6]
+        }
+        datas.append(d)
+    return render_template('index.html', datas=datas)
+
+@app.route('/movie_daily_ranking')
+def movie_daily_ranking():
+    query = "SELECT DISTINCT date FROM daily_movie_ranking;"
+    result = db.execute_fetch(query)
+    datas = []
+    for d in result:
+        d = {'date': d[0]}
+        datas.append(d)
+    return render_template('day_list.html', datas=datas)
+
+@app.route('/movie_daily_ranking/<date>')
+def movie_daily_ranking_detail(date):
+    query = '''
+        SELECT daily_movie_ranking.ranking, movie_data.name, movie_data.grade, movie_data.reservation_percente, movie_data.img_url, 
+               movie_data.short_summary, movie_data.url
+        FROM daily_movie_ranking
+        JOIN movie_data ON daily_movie_ranking.movie_id = movie_data.id
+        WHERE date=?
+        ORDER BY daily_movie_ranking.ranking ASC;
+    '''
+    result = db.execute_fetch(query, (date,))
+    
+    datas = []
+    for d in result:
+        d = {
+            'ranking': d[0],
+            'name': d[1],
+            'grade': d[2],
+            'reservation_percente': d[3],
+            'img_url': d[4],
+            'short_summary': d[5],
+            'url': d[6]
+        }
+        datas.append(d)
+    return render_template('movie_daily_ranking.html', date=date, datas=datas)
 
 @app.route('/update')
 def update():
-    movie_data = get_movie_data()
+    movie_data = get_movie_ranking_data()
     for d in movie_data:
         name = d['name']
         grade = d['grade']
-        reservation_percente = d['reservation_percente']
+        reservation_percente = d['reservation_percente'].replace("%", "")
         img_url = d['img_url']
         short_summary = d['short_summary']
+        url = d['url']
+        ranking = d['ranking']
+        today_date = d['today_date']
         
-        sql = "INSERT INTO movie_data(name, grade, reservation_percente, img_url, short_summary) VALUES(?, ?, ?, ?, ?)"
-        db.execute(sql, (name, grade, reservation_percente, img_url, short_summary))
+        # movie_data 추가
+        query = "SELECT * FROM movie_data WHERE name=?;"
+        movie = db.execute_fetch(query, (name,))
+        if movie is None:
+            query = "INSERT INTO movie_data(name, grade, reservation_percente, img_url, short_summary, url) VALUES(?, ?, ?, ?, ?, ?);"
+            db.execute(query, (name, grade, reservation_percente, img_url, short_summary, url))
+
+        # daily_movie_ranking 추가
+        query = '''
+            SELECT *
+            FROM daily_movie_ranking
+            JOIN movie_data ON daily_movie_ranking.movie_id = movie_data.id
+            WHERE movie_data.name=? AND daily_movie_ranking.date=?;
+        '''
+        movie_ranking = db.execute_fetch(query, (name, today_date))
+        if movie_ranking is None:
+            query = "SELECT id FROM movie_data WHERE name=?;"
+            movie_id = db.execute_fetch(query, (name,))[0][0]
+            query = "INSERT INTO daily_movie_ranking(date, movie_id, ranking) VALUES(?, ?, ?);"
+            db.execute(query, (today_date, movie_id, ranking))
+        
     db.commit()
     return redirect(url_for('index'))
     
--- 14.web_scraping/movie.py
+++ 14.web_scraping/movie.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import date
 
 
-def get_movie_data():
+def get_movie_ranking_data():
     movie_url = "https://movie.daum.net"
     ranking_url = "/ranking/reservation"
     
@@ -18,33 +19,24 @@ def get_movie_data():
         
         name = movie_thumb_cont.select_one('.tit_item > a').text
         grade = movie_thumb_cont.select_one('.txt_grade').text
-        reservation_percente = movie_thumb_cont.select_one('.info_txt > .txt_num').text[:-1]
+        reservation_percente = movie_thumb_cont.select_one('.info_txt > .txt_num').text
         img_url = movie.select_one('.img_thumb')['src']
         short_summary = movie_info.text.strip()
+        url = f"{movie_url}{movie_thumb_cont.select_one('.tit_item > a')['href']}"
+        ranking = movie.select_one('.rank_num').text
+        today_date = date.today()
         
         movie_data.append({
             'name': name,
             'grade': grade,
             'reservation_percente': reservation_percente,
             'img_url': img_url,
-            'short_summary': short_summary
+            'short_summary': short_summary,
+            'url': url,
+            'ranking': ranking,
+            'today_date': today_date,
         })
     return movie_data
-    
-        # sql = "INSERT INTO movie(name, grade, reservation_percente, img_url, short_summary) VALUES(?, ?, ?, ?, ?)"
-        # db.execute(sql, (name, grade, reservation_percente, img_url, short_summary))
-        # db.commit()
-        
-        # print("-"*10)
-        # print(name)
-        # print(grade)
-        # print(reservation_percente)
-        # print(img_url)
-        # print(short_summary)
-        # print("-"*10)
-        # pass
-
-
 
 if __name__ == '__main__':
-    print(get_movie_data())
+    print(get_movie_ranking_data())
