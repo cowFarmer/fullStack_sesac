a0ef31c2c603ed3728a189b8bf4dd8111475f4e0
hou <41228208+cowFarmer@users.noreply.github.com>
2023-08-09 16:14:55 +0900

    Feat: 연결 url 변경, 코드 직관적이게 수정, data update 기능 추가 - index.html url 제거 후 '/' 접속 시 바로 day_list.html 뜨게 수정 - movie 조회 length 보고 데이터 추가 - data scraping 후 daily data 추가하는 버튼 추가

--- 14.web_scraping/app.py
+++ 14.web_scraping/app.py
@@ -1,30 +1,14 @@
 from flask import Flask, render_template, redirect, url_for
 from database import Database
 from movie import get_movie_ranking_data
+from datetime import date
+from papago import translation_ko_to_en
 
 
 app = Flask(__name__)
 db = Database()
 
 @app.route('/')
-def index():
-    query = "SELECT * FROM movie_data;"
-    result = db.execute_fetch(query)
-    datas = []
-    for d in result:
-        d = {
-            'id': d[0], 
-            'name': d[1], 
-            'grade': d[2],
-            'reservation_percente': d[3], 
-            'img_url': d[4], 
-            'short_summary': d[5], 
-            'url': d[6]
-        }
-        datas.append(d)
-    return render_template('index.html', datas=datas)
-
-@app.route('/movie_daily_ranking')
 def movie_daily_ranking():
     query = "SELECT DISTINCT date FROM daily_movie_ranking;"
     result = db.execute_fetch(query)
@@ -76,7 +60,7 @@ def update():
         # movie_data 추가
         query = "SELECT * FROM movie_data WHERE name=?;"
         movie = db.execute_fetch(query, (name,))
-        if movie is None:
+        if len(movie) == 0:
             query = "INSERT INTO movie_data(name, grade, reservation_percente, img_url, short_summary, url) VALUES(?, ?, ?, ?, ?, ?);"
             db.execute(query, (name, grade, reservation_percente, img_url, short_summary, url))
 
@@ -88,14 +72,14 @@ def update():
             WHERE movie_data.name=? AND daily_movie_ranking.date=?;
         '''
         movie_ranking = db.execute_fetch(query, (name, today_date))
-        if movie_ranking is None:
+        if len(movie_ranking) == 0:
             query = "SELECT id FROM movie_data WHERE name=?;"
             movie_id = db.execute_fetch(query, (name,))[0][0]
             query = "INSERT INTO daily_movie_ranking(date, movie_id, ranking) VALUES(?, ?, ?);"
             db.execute(query, (today_date, movie_id, ranking))
         
     db.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('movie_daily_ranking'))
     
 if __name__ == '__main__':
     app.run(debug=True, port=8008)
