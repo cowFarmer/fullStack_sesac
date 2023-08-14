c52d6e202f22d162d4092c656870d9910f873cfa
hou <41228208+cowFarmer@users.noreply.github.com>
2023-08-14 12:16:15 +0900

    Feat: 네이버 소셜 로그인 구현 - 네이버 소셜 로그인 - flask_login이 아닌 session에 직접 남기게 수정 - id를 UUID가 아닌 소셜 로그인시 오는 id로 변경 - password 컬럼 삭제 - birthdate 컬럼 삭제 - 로그인시 바로 id값 보이도록 html 수정

--- project/login/app.py
+++ project/login/app.py
@@ -1,29 +1,24 @@
 from flask import Flask, render_template, redirect, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import requests, random, string, enum, datetime, jwt, uuid
+import requests, enum, datetime, uuid
 
 
 app = Flask(__name__)
 app.secret_key = open("./secret/app_secret_key.txt", "r").read()
-
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///today-exhibition.db"
 db = SQLAlchemy(app)
 
 app.config["SESSION_TYPE"] = "sqlalchemy"
 app.config["SESSION_SQLALCHEMY"] = db
-app.config["SESSION_COOKIE_NAME"] = "TE_SES"
 Session(app)
 
 class LoginType(enum.Enum):
     NAVER = "NAVER"
     KAKAO = "KAKAO"
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.String(64), primary_key=True)
     # password = db.Column(db.String(64))
@@ -34,31 +29,45 @@ class User(db.Model, UserMixin):
     gender = db.Column(db.String(6))
     birthdate = db.Column(db.Date())
     login_type = db.Column(db.Enum(LoginType), nullable=False)
+    
+    def __repr__(self):
+        return f'{self.id},{self.email}{self.nickname}{self.profile_img}{self.created_at}{self.gender}{self.birthdate}{self.login_type}'
 
 @app.route('/')
 def index():
+    if "user_id" in session:
+        user_id = session.get("user_id")
+        return render_template("main.html", user_id=user_id)
     return render_template('main.html')
 
 @app.route('/login', methods=['POST'])
 def login():
     # 현재는 session_data의 유무로 보고 있지만, db랑 조회하는 방식으로 가야하나?
-    if not "_user_id" in session:
-        if 'naver_login' in request.form:
+    if not "user_id" in session:
+        if "naver_login" in request.form:
             NAVER_CLIENT_ID = open("./secret/naver_client_id.txt", "r").read()
-            callback_url = "http://localhost:8888/naver/callback"
-            url = f"https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id={NAVER_CLIENT_ID}&redirect_uri={callback_url}"
+            
+            callback_url = "http://127.0.0.1:8000/naver/callback"
+            url = f"https://nid.naver.com/oauth2.0/authorize?client_id={NAVER_CLIENT_ID}&response_type=code&redirect_uri={callback_url}"
             
             # naver 검증용 state(optional)
             # state = ''.join(random.choice(string.ascii_letters) for _ in range(16))
             # url = f"https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id={NAVER_CLIENT_ID}&redirect_uri={callback_url}&state={state}"
             return redirect(url)
-        if 'kakao_login' in request.form:
+        
+        if "kakao_login" in request.form:
             KAKAO_CLIENT_ID = open("./secret/kakao_client_id.txt", "r").read()
-            callback_url = "http://localhost:8888/kakao/callback"
+            callback_url = "http://127.0.0.1:8000/kakao/callback"
             url = f"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={KAKAO_CLIENT_ID}&redirect_uri={callback_url}"
             return redirect(url)
     else:
         return redirect(url_for('whoami'))
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    if "user_id" in session:
+        session.clear()
+    return redirect(url_for('index'))
     
 @app.route('/naver/callback')
 def naver_callback():
@@ -68,12 +77,14 @@ def naver_callback():
     
     params = request.args.to_dict()
     code = params.get("code")
-    state = params.get("state")
     
-    token_request = requests.get(f"https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id={NAVER_CLIENT_ID}&client_secret={NAVER_CLIENT_SECRET}&code={code}&state={state}")
+    token_request = requests.get(f"https://nid.naver.com/oauth2.0/token?client_id={NAVER_CLIENT_ID}&client_secret={NAVER_CLIENT_SECRET}&grant_type=authorization_code&code={code}")
     token_json = token_request.json()
-    
-    return ""
+    ACCESS_TOKEN = token_json.get("access_token")
+    TOKEN_TYPE = token_json.get("token_type")
+    profile_request = requests.get(f"https://openapi.naver.com/v1/nid/me", headers={"Authorization": f"{TOKEN_TYPE} {ACCESS_TOKEN}"})
+    data = profile_request.json()
+    return social_signin(data, "naver")
 
 @app.route('/kakao/callback')
 def kakao_callback():
@@ -84,7 +95,7 @@ def kakao_callback():
     params = request.args.to_dict()
     code = params.get("code")
     
-    callback_url = "http://localhost:8888/kakao/callback"    
+    callback_url = "http://127.0.0.1:8000/kakao/callback"    
     
     token_request = requests.get(f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={KAKAO_CLIENT_ID}&client_secret={KAKAO_CLIENT_SECRET}&redirect_uri={callback_url}&code={code}")
     token_json = token_request.json()
@@ -96,48 +107,56 @@ def kakao_callback():
 
 @app.route('/whoami')
 def whoami():
-    user_id = session.get("_user_id")
+    user_id = session.get("user_id")
     user = User.query.filter_by(id=user_id).first()
-    return f"<h1>hi {user.nickname}</h1>"
-
-@login_manager.user_loader
-def load_user(user_id):
-    return ""
+    print(user)
+    return f"<h1>{user}</h1>"
 
 def social_signin(data, type):
     if type == "kakao":
-        kakao_account = data.get("kakao_account")
-        profile = kakao_account.get("profile")
-        email = kakao_account.get("email")
+        account_info = data.get("kakao_account")
+        profile = account_info.get("profile")
         
-        # 이메일로 검증
-        user = User.query.filter_by(email=email).first()
-        if not user:
-            new_user = User(
-                id = str(uuid.uuid4()),
-                # password는 넣지 않는거로...
-                # password = str(data.get("id")),
-                email = email,
-                nickname = profile.get("nickname"),
-                profile_img = profile.get("profile_image_url"),
-                created_at = datetime.datetime.now(),
-                gender = kakao_account.get("gender"),
-                # 카카오 birthdate는 월, 일만 받아오는 이슈 있음
-                # birthdate = None,
-                login_type = LoginType.KAKAO
-            )
+        id = data.get("id")
+        email = account_info.get("email")
+        nickname = profile.get("nickname")
+        profile_img = profile.get("profile_image_url"),
+        created_at = datetime.datetime.now(),
+        gender = account_info.get("gender"),
+        login_type = LoginType.KAKAO
+    
+    elif type == "naver":
+        account_info = data.get("response")
         
-            db.session.add(new_user)
-            db.session.commit()
-        
-        user_id = db.session.query(User.id).filter_by(email=email).first()[0]
-        if user_id:
-            user = User.query.get(user_id)
-            login_user(user)
+        id = account_info.get("id")
+        email = account_info.get("email")
+        nickname = account_info.get("nickname")
+        profile_img = account_info.get("profile_image")
+        created_at = datetime.datetime.now()
+        gender = "male" if account_info.get("gender") == "M" else "female"
+        login_type = LoginType.NAVER
+    
+    # 신규 유저 등록
+    user = User.query.filter_by(id=id).first()
+    if not user:
+        new_user = User(
+            id = id,
+            email = email,
+            nickname = nickname,
+            profile_img = profile_img,
+            created_at = created_at,
+            gender = gender,
+            login_type = login_type
+        )
+        db.session.add(new_user)
+        db.session.commit()
+    user_id = db.session.query(User.id).filter_by(email=email).first()[0]
+    if user_id:
+        session["user_id"] = user_id
     
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=8888)
+    app.run(debug=True, port=8000)
